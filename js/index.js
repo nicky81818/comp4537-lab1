3314a92383427366d14cba178c5355899999295f
NICOLE_HSU <nicole.d.hsu@gmail.com>
2025-09-14 14:42:37 -0700

    wrap in app class

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
-document.getElementById("writer-link").innerHTML = STRINGS.LINKS.WRITER;
-document.getElementById("reader-link").innerHTML = STRINGS.LINKS.READER;
+class App {
+  main(){
+    this.applyTranslations();
+  }
+  
+  applyTranslations(){
+    document.getElementById("writer-link").innerHTML = STRINGS.LINKS.WRITER;
+    document.getElementById("reader-link").innerHTML = STRINGS.LINKS.READER;
+  }
+}
